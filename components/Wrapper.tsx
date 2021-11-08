2c41b002be6de4d6a173050af714fc40b6432333
Christian O'Neill <oneill.christian97@gmail.com>
2021-11-08 15:53:42 +0100

    Add navbar

---
+++ components/Wrapper.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Navbar, Footer } from "components";
+import { Flex } from "@chakra-ui/layout";
+
+interface Props {
+	children?: React.ReactNode;
+}
+
+const Wrapper = ({ children }: Props) => {
+	return (
+		<Flex direction="column" minH="100vh">
+			<Navbar />
+			<Flex as="main" direction="column" flex="auto">
+				{children}
+			</Flex>
+			<Footer />
+		</Flex>
+	);
+};
+
+export default Wrapper;
